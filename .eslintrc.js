ee029e827895bc5b2d4119d070ba722c58ba0b56
jaaanix <janis.saritzoglou@opitz-consulting.de>
2016-02-04 18:52:17 +0100

    adds eslint rules and applies them on all js-files

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,12 @@ module.exports = {
     'extends': 'standard',
     'plugins': [
         'standard'
-    ]
+    ],
+    'rules': {
+        'semi': [2, 'always'],
+        'no-mixed-spaces-and-tabs': [2, 'smart-tabs'],
+        'space-before-function-paren': [2, 'never'],
+        'brace-style': [2, 'stroustrup'],
+        'indent': 2
+    }
 };
